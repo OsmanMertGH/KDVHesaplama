3aa6970e56e919dea51a02f6618dd9c7033d2936
OsmanMertGH <osman_mrt@yahoo.com>
2022-12-07 11:29:53 +0300

    1000 TL e göre revize edildi.

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner deger=new Scanner(System.in);
 
+
         System.out.print("Ürün Fiyatı:");
         double fiyat=deger.nextDouble();
-
-        System.out.println("Ürün KDV Dahil Fiyatı:"+fiyat*1.18);
-        System.out.println("Ürün KDV'si:"+fiyat*0.18);
-
+        if(fiyat<1000) {
+            System.out.println("Ürün KDV Dahil Fiyatı:" + fiyat * 1.18);
+            System.out.println("Ürün KDV'si:" + fiyat * 0.18);
+        }
+        else {
+            System.out.println("Ürün KDV Dahil Fiyatı:" + fiyat * 1.08);
+            System.out.println("Ürün KDV'si:" + fiyat * 0.08);
+        }
 
     }
 }
